9b3fd87188089715ff5918ac0d739d12ac70ca6e
nellihol <46727219+nellihol@users.noreply.github.com>
2019-09-11 21:01:29 +1000

    Add files via upload

---
+++ SeatBookingApp/Passenger.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace SeatBookingApp
+{
+    class Passenger
+    {
+        // The data attributes for Passenger
+        public int seatnumber { get; set; }
+        public string passengerName { get; set; }
+        private int securityNumber { get; set; }
+        public string destination { get; set; }
+        public string flightNumber { get; set; }
+        public string departureTime { get; set; }
+        public string departureGate { get; set; }
+
+
+        // Max seats is 40 + 1 to get the numbers work correctly
+        private static int MAXSeats = 41;
+
+        // Array for storing passengers
+        private static Passenger[] allPassengers = new Passenger[MAXSeats];
+
+        public Passenger()
+        {
+            // Set the security number between 3000 and 99999
+            Random random = new Random();
+            securityNumber = random.Next(30000, 999999);
+        }
+
+        // Return a message 
+        public new string ToString()
+        {
+            // Printing seat, name and security
+            return ("- Seat: " + seatnumber + ", Name: " + passengerName + ", Security number: " + securityNumber);
+        }
+
+        // Method for going through the passenger array 
+        public static string AvailableSeats()
+        {
+            string result = "";
+            int index = 0;
+
+            for (int i = 1; i < (allPassengers.Length); i++)
+            {
+                if (allPassengers[i] == null)
+                {
+                    result += i + ", ";
+                }
+                else
+                {
+                    index++;
+                }
+            }
+
+            return result;
+        }
+
+        public static bool IsFlightFull()
+        {
+            // Boolean that goes through the Passenger array and counts them
+            // If it's equal to maximum the flight is full.
+            int index = 0;
+
+            for (int i = 0; i < (allPassengers.Length); i++)
+            {
+                if (allPassengers[i] != null)
+                {
+                    index++;
+                }
+            }
+
+            if (index == MAXSeats)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        // Method for checking if the seat is taken
+        public static bool isSeatTaken(int seatToCheck)
+        {
+            if (allPassengers[0] == null)
+            {
+                return false;
+            }
+            else
+            {
+                for (int i = 0; i < allPassengers.Length; i++)
+                {
+                    if (allPassengers[i] == null)
+                    {
+                        return false;
+                    }
+                    else if (allPassengers[i].seatnumber == seatToCheck)
+                    {
+                        WriteLine("Seat number {0} is taken", allPassengers[i].seatnumber);
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        // Method for showing the passenger list
+        public static string ShowPassengerList()
+        {
+            string passengerlist = "";
+
+            for (int i = 1; i < allPassengers.Length; i++)
+            {
+                if (allPassengers[i] != null)
+                {
+                    passengerlist += allPassengers[i].ToString() + "\n";
+                }
+            }
+            return passengerlist;
+        }
+
+        // Method for booking seats
+        public static void BookASeat(string destination, string date, string flightNumber, string gate)
+        {
+            bool bookingsession = true;
+
+            // Looping the passenger input for this flight until the user says "n"
+            while (bookingsession)
+            {
+                Passenger psngr = new Passenger();
+                // Adding flight details
+                psngr.destination = destination;
+                psngr.flightNumber = flightNumber;
+                psngr.departureGate = gate;
+                psngr.departureTime = date;
+
+                // First Name
+                WriteLine("Enter first name:");
+                string nameInput = ReadLine();
+                if (nameInput.Length > 5)
+                {
+                    nameInput = nameInput.Remove(5);
+                    psngr.passengerName = nameInput;
+                }
+                else
+                {
+                    psngr.passengerName = nameInput;
+                }
+
+                // Last name
+                WriteLine("Enter last name:");
+                string nameInput2 = ReadLine();
+                if (nameInput2.Length > 5)
+                {
+                    nameInput2 = nameInput2.Remove(5);
+                    psngr.passengerName += "/" + nameInput2;
+                }
+                else
+                {
+                    psngr.passengerName += "/" + nameInput2;
+                }
+
+                WriteLine("Enter desired seat number, available seats: " + AvailableSeats());
+
+                // Seat numbers
+                int AllocateSeat;
+                int.TryParse(ReadLine(), out AllocateSeat);
+
+                int userErrors = 0;
+
+                while (AllocateSeat <= 0 || AllocateSeat > (MAXSeats - 1) || isSeatTaken(AllocateSeat))
+                {
+                    WriteLine("Invalid input. Try again");
+                    userErrors++;
+                    int.TryParse(ReadLine(), out AllocateSeat);
+                    if (userErrors > 4)
+                    {
+                        WriteLine("Too many errors");
+                        return;
+                    }
+                }
+
+                psngr.seatnumber = AllocateSeat;
+
+                allPassengers[psngr.seatnumber] = psngr;
+                allPassengers[0] = psngr;
+
+                psngr.ShowBoardingPass();
+
+                WriteLine("Book another seat? Y/N");
+                string userInput = ReadLine();
+
+                if (userInput == "y" || userInput == "Y")
+                {
+                    // Just redo the loop if Y, or warning if flight is full
+                    if (IsFlightFull())
+                    {
+                        WriteLine("Flight is full! no more seats available");
+                        bookingsession = false;
+                    }
+                }
+                else if (userInput == "n" || userInput == "N")
+                {
+                    WriteLine("Thanks for booking!");
+                    bookingsession = false;
+                }
+                else
+                {
+                    WriteLine("I take that as no");
+                    bookingsession = false;
+                }
+            }
+
+            //Date and Time should be printed when passenger entries are completed. 
+            DateTime currTimeAndDate = DateTime.Now;
+            WriteLine("\nThe time and date now is {0}\n", currTimeAndDate);
+        }
+
+        public void ShowBoardingPass()
+        {
+            // Printing boarding pass in tabular format 
+            WriteLine("-------------------------------------------------");
+            WriteLine("* * * * * * B O A R D I N G   P A S S * * * * * *");
+            WriteLine("{0,11} | {1,4} | {2,15}", "fName/lName", "Seat", "security number");
+            WriteLine("{0,11} | {1,4} | {2,15}", passengerName, seatnumber, securityNumber);
+            WriteLine("\n{0,11} | {1,6} | {2,4} | {3,9}", "Destination", "Flight", "Gate", "Date/Time");
+            WriteLine("{0,11} | {1,6} | {2,4} | {3,9}", destination, flightNumber, departureGate, departureTime);
+            WriteLine("-------------------------------------------------");
+        }
+
+        // Method for showing the passenger list
+        public static void ShowAllBoardingPasses()
+        {
+            for (int i = 1; i < allPassengers.Length; i++)
+            {
+                if (allPassengers[i] != null)
+                {
+                    allPassengers[i].ShowBoardingPass();
+
+                }
+            }
+
+        }
+    }
+}
---
+++ SeatBookingApp/Program.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace SeatBookingApp
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            bool endProgram = false;
+            bool isValid = false;
+            bool isStaff = false;
+            int userErrors = 0;
+
+            WriteLine("IFN501 Assignment 2 - Deliverable 2. Nelli Holopainen n10309152");
+            WriteLine("---------------------------------------------------------------");
+
+            // 1. Asking the user type
+            while (isValid != true)
+            {
+                WriteLine("--- Welcome to the Seat booking systecm ---");
+                WriteLine("Who are you?  \n" +
+                        "(1) Airline staff \n" +
+                        "(2) Passenger\n");
+                string userInput2 = ReadLine();
+
+                switch (userInput2)
+                {
+                    case "1":
+                        isStaff = true;
+                        isValid = true;
+                        break;
+                    case "2":
+                        isStaff = false;
+                        isValid = true;
+                        break;
+                    default:
+                        WriteLine("Try again: 1 or 2");
+                        userErrors++;
+                        if (userErrors > 4) {
+                            WriteLine("Too many errors, exiting...");
+                            ReadLine();// Delay to see the message
+                            System.Environment.Exit(0);
+                        }
+                        userInput2 = ReadLine();
+
+                        break;
+                }
+            }
+
+            // reset
+            userErrors = 0;
+            isValid = false;
+
+            // 2. Asking for flight details
+
+            WriteLine("---- Please input the flight details ----");
+
+            WriteLine("Enter Destination:");
+            string destination = ReadLine();
+
+            if (destination.Length <= 2)
+            {
+                // if destination is less than 2 chars
+                // add extra char for not getting errors when generating flight number
+                destination += " ";
+            }
+
+            WriteLine("Departure time and date");
+            WriteLine("Leave today? Y/N");
+            string userInput = ReadLine();
+            string departureTime = "";
+
+            switch (userInput)
+            {
+                case "Y":
+                case "y":
+                    departureTime = DateTime.Now.AddHours(4).ToString(); // Adding 4 hours for convenience
+                    break;
+                case "N":
+                case "n":
+                    // Validating the date input 
+                    while (!isValid)
+                    {
+                        WriteLine("Enter departure date and time in following format \n MM/DD/YYY HH:MM (eg 12/08/2019 18:30)");
+                        DateTime userDateTime;
+                        if (DateTime.TryParse(Console.ReadLine(), out userDateTime))
+                        {
+                            departureTime = "" + userDateTime;
+                            isValid = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("You have entered an incorrect value. Try again");
+                            userErrors++;
+                        }
+                    }
+                    break;
+                default:
+                    WriteLine("I take that as yes");
+                    departureTime = DateTime.Now.AddHours(4).ToString();  // Adding 4 hours for convenience
+                    break;
+            }
+
+            WriteLine("--Thank you!--");
+
+            // Now that the flight is selected, let's generate the flight number and gate number
+
+            Random flightRandom = new Random();
+            // Two first letters of destination + random number = flightnumber
+            string flightNumber = destination.Substring(0, 2).ToUpper() + flightRandom.Next(100, 999).ToString();
+            WriteLine("\nThe flight number " + flightNumber + " to " + destination + " selected");
+
+
+            Random gateRandom = new Random();
+            int gateNumber = gateRandom.Next(1, 45);
+            string gate = gateNumber.ToString();
+
+            if (gateNumber % 2 == 0)
+            {
+                gate += "a";
+            }
+            else if (gateNumber > 35)
+            {
+                gate += "b";
+            }
+
+            WriteLine("The flight departs " + departureTime + " at gate " + gate + "\n");
+
+            bool isFlightFull = Passenger.IsFlightFull();
+            userErrors = 0;
+
+            // 3. Start booking seats for the chosen flight, end the loop if flight is full
+            while (endProgram != true)
+            {
+                // Check what's the user role and show correct text 
+                if (isStaff)
+                {
+                    WriteLine("--- What would you like to do now? ---  \n" +
+                                       "(1) Input customer details & Book seats \n" +
+                                       "(2) Show available seats\n" +
+                                       "(3) Show all passengers. \n" +
+                                       "Press Q to exit.");
+                }
+                else
+                {
+                    WriteLine("--- What would you like to do now? --- \n" +
+                               "(1) Input customer details & Book seats  \n" +
+                               "(2) Show available seats\n" +
+                               "(3) Show boarding passes \n" +
+                               "Press Q to exit.");
+                }
+
+                string userInput3 = ReadLine();
+
+                //Starting the main loop
+                switch (userInput3)
+                {
+                    case "q":
+                    case "Q":
+                        endProgram = true;
+                        break;
+
+                    case "1":
+                        Passenger.BookASeat(destination, departureTime, flightNumber, gate);
+                        break;
+                    case "2":
+                        // Show the Available seats
+                        WriteLine("Available seat numbers: " + Passenger.AvailableSeats());
+                        break;
+                    case "3":
+                        // Show list of all passengers if the user is staff
+                        if (isStaff)
+                        {
+                            if (Passenger.ShowPassengerList() != "")
+                            {
+                                // Print passengers  if there is passengers
+                                WriteLine("Passenger list:");
+                                WriteLine(Passenger.ShowPassengerList());
+                            }
+                            else
+                            {
+                                WriteLine("No pssengers yet. Select option 1 to add.");
+                            }
+                        }
+                        if (!isStaff)
+                        {
+                            // Show the booked boarding passes if user is a passenger
+                            Passenger.ShowAllBoardingPasses();
+                            WriteLine("Please select option 1 to enter client details");
+                        }
+                        break;
+                    // Default: ask for new input
+                    default:
+                        WriteLine("Try again.");
+                        userErrors++;
+                        break;
+                }
+
+                if (userErrors > 4) {
+                    WriteLine("Too many errors. Exiting...");
+                    endProgram = true;
+                }
+                // Check every time if the flight is full
+                isFlightFull = Passenger.IsFlightFull();
+                if (isFlightFull)
+                {
+                    endProgram = true;
+                }
+            }
+
+            if (isFlightFull)
+            {
+                WriteLine("\n\n---Checking closed - the flight is full--");
+
+                if (isStaff)
+                {
+                    WriteLine("Showing all customers ...");
+                    Passenger.ShowPassengerList();
+                }
+                else
+                {
+                    WriteLine("Showing all boarding passes booked in this session...");
+                    Passenger.ShowAllBoardingPasses();
+                }
+            }
+
+            ReadLine();
+        }
+    }
+}
